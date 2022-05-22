c5c756eb113ef9563374dacba0ce31e2a472b442
Tamirlan <tuarshintamir@gmail.com>
2022-05-22 14:20:55 +0500

    Двумерный массив с упорядоченными по убыванию числами

---
+++ Zadacha54/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Console.WriteLine("Hello, World!");
+void FillArray (int [,] array,int minValue, int maxValue)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array [i,j] =new Random().Next(1,10);
+        }
+    }
+}
+
+void PrintArray(int [,] array)
+{
+   for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++) 
+        Console.Write (array[i,j]+ " ");
+        Console.WriteLine();
+    }
+}
+
+void SortArray(int[,]array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    for (int j = 0; j < array.GetLength(1) - 1; j++)
+        for (int k = j + 1; k < array.GetLength(1); k++)
+        {
+            if (array[i, k] > array[i, j])
+            {
+                int temp = array[i, j];
+                array[i, j] = array[i, k];
+                array[i, k] = temp;
+            }
+        }
+}   
+int [,] array = new int [2,3];
+FillArray(array, 1,10);
+PrintArray(array);
+System.Console.WriteLine();
+SortArray(array);
+PrintArray(array);
