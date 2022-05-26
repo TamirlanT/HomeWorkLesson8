793365136be45766c1b9cc7f0b7b2a19b9d329f1
Tamirlan <tuarshintamir@gmail.com>
2022-05-26 13:28:08 +0500

    Задача с наименьшей суммой строки

---
+++ Zadacha56/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте прямоугольный двумерный массив. 
+//Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Console.WriteLine("Hello, World!");
+
+int [,] array = {  {9,9,9},
+                    {1,6,7},
+                    {5,6,7,},
+                    {2,2,2}};
+int sum = int.MaxValue;
+int index = 0;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    int temp = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        temp += array[i, j];
+    }
+    if (temp < sum)
+    {
+        sum = temp;
+        index = i;
+    }
+}
+Console.WriteLine("Строка: "+index+" Сумма : "+sum);
+Console.WriteLine();
+for (int i = 0; i < 3; i++)
+{
+Console.Write(array[index, i]+" "); 
+}
+ 
